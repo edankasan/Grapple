5b88e621fd6ebeece3fa5bf273bc9bba9daaba67
edan kasan <34835563+edankasan@users.noreply.github.com>
2018-01-05 10:29:12 +0200

    fucking doors
    fucking doors

---
+++ Assets/Scripts/DoorWarp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorWarp : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("ding");
+    }
+}
