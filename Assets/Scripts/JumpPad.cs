62e131970fd0e5cf29bd2278b7e09b3fd73a4f27
edan kasan <34835563+edankasan@users.noreply.github.com>
2018-02-10 19:53:46 +0200

    JUMPY BOI
    I AM A JUMPY BOI

---
+++ Assets/Scripts/JumpPad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour {
+    protected Rigidbody2D rb2d;
+    public float jumppower;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("ding");
+        collision.collider.GetComponent<Rigidbody2D>().velocity = new Vector2(collision.otherCollider.GetComponent<Rigidbody2D>().velocity.x, collision.otherCollider.GetComponent<Rigidbody2D>().velocity.y + jumppower);
+    }
+}
