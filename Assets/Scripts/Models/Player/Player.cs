fb41b21c0e0ad531fe7049a0d699e0e197959da0
Sean Galimidi <totalsgalimidi@gmail.com>
2018-02-20 21:44:57 +0200

    Revert "boop"
    This reverts commit 0ac10236d75918ecedcfa2193c5807406fb199ed.

---
+++ Assets/Scripts/Models/Player/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class Player
+{
+    public string Name { get; protected set; }
+
+    public Rect Rect { get; protected set; }
+
+    public Player(Rect rect, string name)
+    {
+        Name = name;
+        Rect = rect;
+    }
+}
