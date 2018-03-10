b9b14708f72f470650e797d0b24b5cd4927a704d
edankasan <edan.kasan@gmail.com>
2018-03-10 19:01:37 +0200

    grappling hook better
    now can drag crates.
    should edit crate sprite to be a perfect square.
    also crate interacts weirdly with player and the grounded bool. must fix!!!!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -61,18 +61,11 @@ public class PlayerController : MonoBehaviour {
 
     void Update()
     {
-        if (!hasDistanceJoint2D(grappleShooter))
-        {
             grounded = false;
             if (rb2d.velocity.y == 0)
             {
                 grounded = true;
             }
-        }
-        else
-        {
-            grounded = false;
-        }
         UpdateAnimator();
 
         InputController.Instance.CheckForInput();
@@ -137,7 +130,7 @@ public class PlayerController : MonoBehaviour {
     #region keyboard control funcs
     public bool OnDHeld()
     {
-        if (!hasDistanceJoint2D(grappleShooter))
+        if (!hasDistanceJoint2D(grappleShooter) || grounded)
         {
             rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
         }
@@ -151,7 +144,7 @@ public class PlayerController : MonoBehaviour {
 
     public bool OnAHeld()
     {
-        if (!hasDistanceJoint2D(grappleShooter))
+        if (!hasDistanceJoint2D(grappleShooter) || grounded)
         {
             rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
         }
@@ -165,7 +158,7 @@ public class PlayerController : MonoBehaviour {
 
     public bool OnWHeld()
     {
-        if (!hasDistanceJoint2D(grappleShooter) && grounded)
+        if (grounded)
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, jumpPower);
         }
@@ -182,7 +175,7 @@ public class PlayerController : MonoBehaviour {
 
     public bool OnSHeld()
     {
-        if(hasDistanceJoint2D(grappleShooter) && rb2d.position.y < grapple.connectedAnchor.y && grapple.distance <= 6.97)
+        if(hasDistanceJoint2D(grappleShooter) && rb2d.position.y < grapple.connectedBody.position.y && grapple.distance <= 6.97)
         {
             grapple.distance = grapple.distance + 0.03f;
         }
@@ -201,7 +194,7 @@ public class PlayerController : MonoBehaviour {
 
     public bool OnAReleased()
     {
-        if (!hasDistanceJoint2D(grappleShooter))
+        if (!hasDistanceJoint2D(grappleShooter) || grounded)
         {
             rb2d.velocity = new Vector2(0, rb2d.velocity.y);
         }
@@ -211,7 +204,7 @@ public class PlayerController : MonoBehaviour {
 
     public bool OnDReleased()
     {
-        if (!hasDistanceJoint2D(grappleShooter))
+        if (!hasDistanceJoint2D(grappleShooter) || grounded)
         {
             rb2d.velocity = new Vector2(0, rb2d.velocity.y);
         }
@@ -245,10 +238,11 @@ public class PlayerController : MonoBehaviour {
         {
             DistanceJoint2D newGrapple = grappleShooter.AddComponent<DistanceJoint2D>();
             newGrapple.enableCollision = false;
-            newGrapple.connectedAnchor = hit.point;
             newGrapple.enabled = true;
             newGrapple.maxDistanceOnly = true;
             newGrapple.connectedBody = hit.collider.GetComponent<Rigidbody2D>();
+            newGrapple.enableCollision = true;
+
 
             GameObject.DestroyImmediate(grapple);
             grapple = newGrapple;
@@ -262,7 +256,7 @@ public class PlayerController : MonoBehaviour {
             lineRenderer.enabled = true;
             lineRenderer.positionCount = 2;
             Vector3 position0 = new Vector3(grappleShooter.transform.position.x, grappleShooter.transform.position.y, -5);
-            Vector3 position1 = new Vector3(grapple.connectedAnchor.x, grapple.connectedAnchor.y, -5);
+            Vector3 position1 = new Vector3(grapple.connectedBody.position.x, grapple.connectedBody.position.y, -5);
             lineRenderer.SetPosition(0, position0);
             lineRenderer.SetPosition(1, position1);
         }
